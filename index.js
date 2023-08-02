bee39fb4d381c115d086c637e77ab03fb963cc46
siva <vsivaprabu.my@gmail.com>
2023-08-02 23:24:56 +0530

    cdn react

---
+++ index.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1",{},[React.createElement("h2",{key:1}," test 1"),React.createElement("h2",{key:2}," test 2")]);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log('root ',root)
+
+root.render(heading)
